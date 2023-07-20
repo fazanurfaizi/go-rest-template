087aeb6338ecfb2267ffd4d71ce04f8aa64c2777
Faza Nurfaizi <nurfaizi.faza@gmail.com>
2023-07-20 09:35:05 +0700

    otp generator helper

---
+++ pkg/helpers/otp_generator_test.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"testing"
+)
+
+func TestGenerateOTPCode(t *testing.T) {
+	length := 6
+
+	code1, err := GenerateOTPCode(length)
+	if err != nil {
+		t.Error("error occurred while generating code 1")
+	}
+
+	code2, err := GenerateOTPCode(length)
+	if err != nil {
+		t.Error("error occurred while generating code 2")
+	}
+
+	if code1 == code2 {
+		t.Error("function have to generate difference code when execute twice ")
+	}
+
+	if len(code1) != length {
+		t.Error("invalid length")
+	}
+}
