117e81e460ba5b873a5535a10a9d531c0f1b65e0
Faza Nurfaizi <nurfaizi.faza@gmail.com>
2023-07-20 09:30:23 +0700

    bcrypt helper

---
+++ pkg/helpers/bcrypt.go
@@ -0,0 +1,25 @@
+package helpers
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GenerateHash(password string) (string, error) {
+	if password == "" {
+		return "", errors.New("password cannot be empty")
+	}
+
+	result, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(result), nil
+}
+
+func ValidateHash(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
---
+++ pkg/helpers/bcrypt_test.go
@@ -0,0 +1,57 @@
+package helpers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGenerateHash(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       string
+		expectedErr bool
+	}{
+		{"Success", "secret", false},
+		{"Error", "", true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			hash, err := GenerateHash(test.input)
+			if (err != nil) != test.expectedErr {
+				t.Errorf("GenerateHash(%q) error = %v, expectedErr %v", test.input, err, test.expectedErr)
+			}
+			if !test.expectedErr {
+				if len(hash) == 0 {
+					t.Errorf("GenerateHash(%q) = %q, expected non-empty string", test.input, hash)
+				}
+			}
+		})
+	}
+}
+
+func TestValidateHash(t *testing.T) {
+	secret := "secret"
+	poorHash := "fail"
+	hash, err := GenerateHash(secret)
+	if err != nil {
+		t.Error(err)
+	}
+	tests := []struct {
+		name    string
+		secret  string
+		hash    string
+		isValid bool
+	}{
+		{"Success", secret, hash, true},
+		{"Invalid", secret, hash[:len(hash)-len(poorHash)] + poorHash, false},
+		{"Invalid", "invalid", "invalid", false},
+	}
+	for index, test := range tests {
+		t.Run(fmt.Sprintf("Test %d | %s", index, test.name), func(t *testing.T) {
+			if got := ValidateHash(test.secret, test.hash); got != test.isValid {
+				t.Errorf("ValidateHash(%q, %q) = %v, expected %v", test.secret, test.hash, got, test.isValid)
+			}
+		})
+	}
+}
