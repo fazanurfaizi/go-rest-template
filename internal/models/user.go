3272dfc499c47a97a6f79ff5689d375a7e445ef4
Faza Nurfaizi <nurfaizi.faza@gmail.com>
2023-07-21 13:01:32 +0700

    chore: add soft deletes to user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,16 +1,18 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 
 	uuid "github.com/google/uuid"
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	ID        uuid.UUID `gorm:"type:uuid;primary_key"`
 	Name      string    `gorm:"type:varchar(255);no null"`
 	Email     string    `gorm:"type:varchar(255);uniqueIndex;not null"`
 	Password  string    `gorm:"type:varchar(255)"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+	DeletedAt sql.NullTime `gorm:"index"`
 }
